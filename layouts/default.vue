21b5f3cb20d3e862ca5695822ca7892adb1812dd
mango-byte <varey@mango-byte.com>
2022-10-10 16:33:14 +0700

    format style

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,11 @@
 <template>
   <v-app dark>
-    <v-app-bar app elevation="0" elevate-on-scroll   style="background-color: #ECEFF1">
+    <v-app-bar
+      app
+      elevation="0"
+      elevate-on-scroll
+      style="background-color: #eceff1"
+    >
       <nuxt-link to="/" style="text-decoration-line: none">
         <v-toolbar-title class="black--text" v-text="title" />
       </nuxt-link>
@@ -9,12 +14,12 @@
         <v-img :src="require('@/assets/images/avatar1.jpg')" />
       </v-avatar>
     </v-app-bar>
-    <v-main style="background-color: #ECEFF1">
+    <v-main style="background-color: #eceff1">
       <v-container style="background-color: #fff" class="pa-10">
         <Nuxt />
       </v-container>
     </v-main>
-    <v-footer fixed app style="background-color: #ECEFF1">
+    <v-footer fixed app style="background-color: #eceff1">
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
